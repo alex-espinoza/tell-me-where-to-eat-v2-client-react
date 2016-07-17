5c6f2957649533f200a83b715e96cd276dda8aeb
Alex Espinoza <aespinoza@aspria.net>
2016-07-16 22:20:09 -0400

    Update webpack.config to use autoprefixer for css

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,38 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var autoprefixer = require('autoprefixer');
+
 module.exports = {
-  entry: './app/App.js',
+  context: __dirname + '/app',
+  entry: {
+    javascript: './App.js',
+    css: './css/style.css'
+  },
   output: {
-    filename: 'public/bundle.js'
+    filename: 'bundle.js',
+    path: __dirname + '/public'
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         loader: 'babel',
         query: {
           presets: ['react', 'es2015']
         }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract(
+            'style',
+            'css!postcss-loader'
+          )
       }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin("style.css")
+  ],
+  postcss: [autoprefixer]
 };
