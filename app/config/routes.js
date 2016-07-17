b6723a6a3ef78cdc60e2ffa82020194e3c0bab1f
Alex Espinoza <aespinoza@aspria.net>
2016-07-17 13:37:08 -0400

    Delete unused Home component

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,6 +1,5 @@
 var React = require('react');
 var Main = require('../components/Main');
-var Home = require('../components/Home');
 var Router = require('react-router');
 var Route = Router.Route;
 var IndexRoute = Router.IndexRoute;
